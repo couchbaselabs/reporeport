0ce5fba3566d91d3e791d7ca381bf4195d519c91
Tommie McAfee <tahmmee@gmail.com>
2017-01-16 18:46:43 +0000

    add more gateway tests from mobile-testkit

--- scripts/mobile_dump.sh
+++ scripts/mobile_dump.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-
 pytest mobile-testkit/testsuites/listener/shared/client_client --collect-only | sed "s/.*test/client:test/g" |  grep -v "\.py" | grep ".>" | sed  "s/'>//" > mobile.conf
-pytest mobile-testkit/testsuites/listener/shared/client_sg --collect-only | sed "s/.*test/gateway:test/g" |  grep -v "\.py" | grep ".>" | sed  "s/'>//" >> mobile.conf
+pytest mobile-testkit/testsuites/listener/shared/client_sg --collect-only | sed "s/.*test/client:test/g" |  grep -v "\.py" | grep ".>" | sed  "s/'>//" >> mobile.conf
+pytest mobile-testkit/testsuites/syncgateway/functional/tests --collect-only | grep Function | sed "s/.*'test/gateway:test/g" |  grep -v "\.py" | grep ".>" | sed  "s/'>//" >> mobile.conf
