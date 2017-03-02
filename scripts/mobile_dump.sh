53d95b9f954bc374c3d22141f4ad05ea27f03df7
Tommie McAfee <tahmmee@gmail.com>
2017-03-02 15:21:31 -0500

    add mobile topology tests

--- scripts/mobile_dump.sh
+++ scripts/mobile_dump.sh
@@ -2,3 +2,4 @@
 pytest mobile-testkit/testsuites/listener/shared/client_client --collect-only | sed "s/.*test/client:test/g" |  grep -v "\.py" | grep ".>" | sed  "s/'>//" > mobile.conf
 pytest mobile-testkit/testsuites/listener/shared/client_sg --collect-only | sed "s/.*test/client:test/g" |  grep -v "\.py" | grep ".>" | sed  "s/'>//" >> mobile.conf
 pytest mobile-testkit/testsuites/syncgateway/functional/tests --collect-only | grep Function | sed "s/.*'test/gateway:test/g" |  grep -v "\.py" | grep ".>" | sed  "s/'>//" >> mobile.conf
+pytest mobile-testkit/testsuites/syncgateway/functional/topology_specific_tests --collect-only | sed "s/.*test/topology:test/g" |  grep -v "\.py" | grep ".>" | sed  "s/'>//" >> mobile.conf
